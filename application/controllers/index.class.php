3cbc0df2511c7b8b04f2b22a814e2f7a4d078c74
Tom Humphris <tom@muska.co.uk>
2014-07-25 14:39:02 +0400

    Getting authentication ready for backbone changes.

--- application/controllers/index.class.php
+++ application/controllers/index.class.php
@@ -8,9 +8,9 @@ class IndexController {
 	public function generateAssets(Request $request, Application $app) {
 		// If not authenticated serve login screen initial assets
 		if(!$app['session']->get('isAuthenticated')) {
-			$template = file_get_contents(DOCUMENT_ROOT.'/application/assets/backbone/ticketapp/index.html');
-		} else {
 			$template = file_get_contents(DOCUMENT_ROOT.'/application/assets/backbone/loginapp/index.html');
+		} else {
+			$template = file_get_contents(DOCUMENT_ROOT.'/application/assets/backbone/ticketapp/index.html');
 		}
 		
 		return $template;
--- application/models/auth.class.php
+++ application/models/auth.class.php
@@ -10,20 +10,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
  
 class BasicAuthControllerProvider implements ControllerProviderInterface {
-	public function register() {
-		
+	public function authenticatePassword() {
+		// Check password matches that stored in mongo
 	}
 	
-	public function login() {
-		
-	}
-	
-	public function authenticated() {
-		return false;
+	public function lookupUser() {
+		// Lookup user in mongo and return user document
 	}
 
     public function connect(Application $app) {
-        // Init
+        // Init - OLD SCHOOL - REPLACE WITH MONGO AUTH
         $app['login.username'] = (isset($app['login.username']))? $app['login.username']: "houston";
         $app['login.password'] = (isset($app['login.password']))? $app['login.password']: "password";
         $app['login.redirect'] = (isset($app['login.redirect']))? $app['login.redirect']: "home";
@@ -34,26 +30,35 @@ class BasicAuthControllerProvider implements ControllerProviderInterface {
             $response->setStatusCode(401, 'Please sign in.');
             return $response;
         };
+        
         // Controllers
         $controllers = $app['controllers_factory'];
         
         // Login
-        $controllers->get('/', function (Request $request, Application $app) {
-            $username = $request->server->get('PHP_AUTH_USER', false);
-            $password = $request->server->get('PHP_AUTH_PW');
- 
+        $controllers->post('/', function (Request $request, Application $app) {
+            //$username = $request->server->get('PHP_AUTH_USER', false);
+            //$password = $request->server->get('PHP_AUTH_PW');
+            $username = $request->server->post('username');
+            $password = $request->server->post('password');
+            
+			// Authenticate! Change to lookupUser and authenticatePassword methods!!!
             if ($app['login.username'] === $username && $app['login.password'] === $password) {
                 $app['session']->set('isAuthenticated', true);
-                //header('Location: /');
-                return $app->redirect($app['url_generator']->generate($app['login.redirect']));
+                
+                //return $app->redirect($app['url_generator']->generate($app['login.redirect']));
+                return $app->redirect('/');
             }
-            return $app['login.basic_login_response'];
+            
+            //return $app['login.basic_login_response'];
+            return $app->redirect('/');
         })->bind('login');
  
         // Logout
         $controllers->get('/logout', function (Request $request, Application $app) {
             $app['session']->set('isAuthenticated', false);
-            return $app['login.basic_login_response'];
+            
+            //return $app['login.basic_login_response'];
+            return $app->redirect('/');
         })->bind('logout');
  
         // Add before event
@@ -69,7 +74,8 @@ class BasicAuthControllerProvider implements ControllerProviderInterface {
             }
             $app['session']->get('isAuthenticated');
             if (!$app['session']->get('isAuthenticated')) {
-                $ret = $app->redirect($app['url_generator']->generate('login'));
+                //$ret = $app->redirect('/');
+                $ret = null;
             } else {
                 $ret = null;
             }
@@ -77,21 +83,5 @@ class BasicAuthControllerProvider implements ControllerProviderInterface {
                 return $ret;
             }
     	});
-    
-        /*$app['dispatcher']->addListener(SilexEvents::BEFORE, function (GetResponseEvent $event) use ($app){
-            $request = $event->getRequest();
-            if ($request->getRequestUri() === $app['url_generator']->generate('login')) {
-                return;
-            }
-            $app['session']->get('isAuthenticated');
-            if (!$app['session']->get('isAuthenticated')) {
-                $ret = $app->redirect($app['url_generator']->generate('login'));
-            } else {
-                $ret = null;
-            }
-            if ($ret instanceof Response) {
-                $event->setResponse($ret);
-            }
-        }, 0);*/
     }
 }
