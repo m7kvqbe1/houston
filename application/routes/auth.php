938df69b2947da6cb0372a9bff62eb3bed3fb351
Tom Humphris <tom@muska.co.uk>
2014-08-06 19:29:19 +0400

    Register new users and login authenticates against users collection.

--- application/routes/auth.php
+++ application/routes/auth.php
@@ -7,21 +7,23 @@ $app->post('/auth/login', function(Request $request, Application $app) {
 	$user = new Houston\User\Model\UserModel();
 
 	session_start();
-
-	$auth = array(
-		'username' => 'houston',
-		'password' => 'password'
-	);
 	
 	$json = json_decode(file_get_contents('php://input'));
 	
-	if($json->user === $auth['username'] && $json->password === $auth['password']) {
-		$app['session']->set('isAuthenticated', true);
-		
-		return 1;
-	} else {
-		return -1;
+	$m = new MongoClient("mongodb://localhost");
+	$db = $m->houston;
+	
+	$criteria = array('emailAddress' => $json->user);
+	$doc = $db->users->findOne($criteria);
+	
+	if(!empty($doc)) {
+		if($json->password === $doc['password']) {
+			$app['session']->set('isAuthenticated', true);
+			return 1;
+		}
 	}
+	
+	return -1;
 });
 
 // Logout of system
@@ -38,5 +40,30 @@ $app->post('/auth/register', function(Request $request, Application $app) {
 	
 	$json = json_decode(file_get_contents('php://input'));
 	
-	return $json;
+	// Setup MongoDB
+	$m = new MongoClient("mongodb://localhost");
+	$db = $m->houston;
+	
+	// Check if user exists and save to database
+	try {
+		$criteria = array('emailAddress' => $json->emailAddress);
+		$doc = $db->users->findOne($criteria);
+		
+		if(!empty($doc)) {
+			return -1;
+		} else {
+			$collection = $db->users;
+			$collection->save($json);
+		}
+		
+		$m->close();
+		
+		$app['session']->set('isAuthenticated', true);
+		
+		return 1;
+	} catch(MongoConnectionException $e) {
+		die('Error connecting to MongoDB server');
+	} catch(MongoException $e) {
+		die('Error: '.$e->getMessage());
+	}
 });
