9f8f7a747e611abd0417980a75f26124baf12a1e
Tom Humphris <tom@muska.co.uk>
2014-08-11 16:49:01 +0400

    Return user object on successful login.

--- application/routes/ticket.php
+++ application/routes/ticket.php
@@ -1,4 +1,8 @@
 <?php
+use Symfony\Component\HttpFoundation\Request;
+use Silex\Application;
+
+// Deprecated!
 $app->get('/tickets', function() {
 	$tickets = new Houston\Ticket\Model\TicketModel();
 	return $tickets->getTickets();
@@ -12,5 +16,11 @@ $app->get('/tickets/{method}', function(Silex\Application $app, $method) {
     if(method_exists($user, $method)) {
     	// Call method dynamically based on route
 		return $user->{$method}('Arg');
-    }    
+    }
+});
+
+// New code!
+$app->post('/ticket/add', function(Request $request, Application $app) {
+	$json = json_decode(file_get_contents('php://input'));
+	var_dump($json);
 });
