afce18da4234a9b676dc8b1a0d9a2861f6cfad80
Tom Humphris <tom@muska.co.uk>
2014-10-23 15:54:27 +0400

    Minor refactoring

--- application/routes/auth.php
+++ application/routes/auth.php
@@ -119,9 +119,7 @@ $app->get('/user/self', function(Request $request, Application $app) {
 	$userModel = new Houston\User\Model\UserModel($app);
 	$userModel->loadUserByID($app['session']->get('u'));
 	
-	if(!isset($userModel->user)) {
-	    return -1;
-	}
+	if(!isset($userModel->user)) return -1;
 	
 	unset($userModel->user['password']);
 	return json_encode($userModel->user);
