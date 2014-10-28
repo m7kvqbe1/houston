20a253e0e8febe06b8fdff6f073b0aaf7fc75a5b
Tom Humphris <tom@muska.co.uk>
2014-10-28 16:13:38 +0300

    Changes to application config.

---
+++ application/config.php
@@ -0,0 +1,13 @@
+<?php
+class Config {	
+	const DOMAIN = 'http://tom.houston.com';
+	const DOMAIN_SSL = 'https://tom.houston.com';
+	
+	const DEFAULT_SALT = 'Adfkjasf93482394!!';
+	
+	const MONGO_HOST = 'mongodb://localhost:27017';
+	const MONGO_USER = '';
+	const MONGO_PASSWORD = '';
+	
+	const ERROR_REPORTING = true;
+}
