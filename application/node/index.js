6317355678df90ff853c817a62b43907097ffd3c
Tom Humphris <tom@muska.co.uk>
2014-11-05 20:38:32 +0300

    Emit socket events for new ticket, reply and ticket status.

--- application/node/index.js
+++ application/node/index.js
@@ -2,19 +2,42 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/views/notification/default.html');
-});
-
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
 
 io.on('connection', function(socket){
-	socket.broadcast.emit('hello world');
-	
-	socket.on('message', function(msg) {
-		console.log(msg);
-		io.emit('message', 'monkey');
+	socket.on('response', function(data) {
+		// Output response from client server side
+		console.log(data);
 	});
+});
+
+// Serve assets to client for Socket
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/views/notification/default.html');
+});
+
+// Broadcast new ticket event
+app.get('/newticket', function(req, res) {
+	console.log('newticket');
+	io.emit('event-newticket', 'New Ticket');
+	
+	res.end();
+});
+
+// Broadcast reply event
+app.get('/reply', function(req, res) {
+	console.log('reply');
+	io.emit('event-reply', 'New Reply');
+	
+	res.end();
+});
+
+// Broadcast status event
+app.get('/status', function(req, res) {
+	console.log('status');
+	io.emit('event-status', 'Status Update');
+	
+	res.end();
 });
