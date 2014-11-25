403bb85e7f047b8f2b2a3a3840e78a8ff4440137
Tom Humphris <tom@muska.co.uk>
2014-11-25 17:25:45 +0300

    Now handling multipart and non multipart emails

--- application/extras/mailbox.class.php
+++ application/extras/mailbox.class.php
@@ -5,7 +5,7 @@ use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 
 class Mailbox {
-	public $inbox;
+	protected $inbox;
 	public $emails = array();
 	
 	public function __construct() {
@@ -28,17 +28,23 @@ class Mailbox {
 		$emails = imap_search($this->inbox, 'ALL');
 		foreach($emails as $num) {
 			$overview = imap_fetch_overview($this->inbox, $num, 0);
+			$structure = imap_fetchstructure($this->inbox, $num);
 			
 			$email['read'] = ($overview[0]->seen ? 'read' : 'unread');
 			$email['subject'] = $overview[0]->subject;
 			$email['from'] = $overview[0]->from;
-			$email['date'] = $overview[0]->date;
-			$email['message'] = imap_fetchbody($this->inbox, $num, 1);
+			$email['date'] = $overview[0]->date;	
+			$email['message'] = ($this->checkType($structure) ? imap_fetchbody($this->inbox, $num, 1) : $email['message'] = imap_body($this->inbox, $num));			
 			
 			array_push($this->emails, $email);
 		}
 		
 		return $this->emails;
+	}
+	
+	public function checkType($structure) {
+		if($structure->type == 1) return true;
+		return false;
 	}
 	
 	public function markRead() {
