e0152ba7d7f76aef572dc26ea6e05db784824b89
Tom Humphris <tom@muska.co.uk>
2014-11-25 15:17:39 +0300

    Added mailbox class

---
+++ application/extras/mailbox.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace Houston\Extra;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+class Mailbox {
+	public $inbox;
+	
+	public function getEmail() {
+		
+	}
+	
+	public function markRead() {
+		
+	}
+	
+	public function markUnread() {
+		
+	}
+}
