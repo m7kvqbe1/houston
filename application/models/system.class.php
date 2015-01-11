bbfab55cf44ceb943456d152d61919dfa7d3eb3e
Tom Humphris <tom@muska.co.uk>
2015-01-11 20:20:11 +0300

    Comment change

--- application/models/system.class.php
+++ application/models/system.class.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 class System {
 	public static function generateAssets(Request $request, Application $app) {
-		// If not authenticated serve login screen initial assets
+		// If session not authenticated serve login screen initial assets
 		if(!$app['session']->get('isAuthenticated')) {
 			$template = file_get_contents(DOCUMENT_ROOT.'/application/assets/backbone/loginapp/index.html');
 		} else {
