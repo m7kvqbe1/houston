068a41b70ec52eb05eaa6099cd883efec028ea80
Tom Humphris <tom@muska.co.uk>
2015-02-12 18:18:35 +0300

    Using libxml_use_internal_errors instead of error supression operator (@) on getMessageMeta method

--- application/components/error.class.php
+++ application/components/error.class.php
@@ -1,2 +1,37 @@
 <?php
 namespace Houston\Components;
+
+class ErrorTrap {
+	protected $callback;
+	protected $errors = array();
+	
+	public function __construct($callback) {
+		$this->callback = $callback;
+	}
+
+	public function call() {
+		$result = null;
+		set_error_handler(array($this, 'onError'));
+		try {
+			$result = call_user_func_array($this->callback, func_get_args());
+		} catch (Exception $ex) {
+			restore_error_handler();        
+			throw $ex;
+		}
+		
+		restore_error_handler();
+		return $result;
+	}
+
+	public function onError($errno, $errstr, $errfile, $errline) {
+		$this->errors[] = array($errno, $errstr, $errfile, $errline);
+	}
+
+	public function ok() {
+		return count($this->errors) === 0;
+	}
+
+	public function errors() {
+		return $this->errors;
+	}
+}
