2ceac760363815e008023329573decb14fd42f6a
Tom Humphris <tom@muska.co.uk>
2015-02-17 14:36:25 +0300

    Added ErrorDefinition class

--- application/components/response.class.php
+++ application/components/response.class.php
@@ -5,35 +5,57 @@ use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class ApiResponse extends Response {
+class ApiResponse extends Response 
+{
 	protected $app;
 	
-	public function __construct(Application $app) {
+	public function __construct(Application $app) 
+	{
 		$this->app = $app;
 	}
 	
-	public function error($code, $customMessage = null) {
+	public function error($code, $customMessage = null) 
+	{
 		if(isset($customMessage)) $message = $customMessage;
 		return self::encodeResponse(array('code' => $code, 'status' => 'error', 'message' => $message));
 	}
 	
-	public function success($code, $customMessage = null) {
-		if(isset($customMessage)) $message = $customMessage;
+	public function success($code, $customMessage = null) 
+	{
+		if(isset($customMessage)) {
+			$message = $customMessage;	
+		} else {
+			$message = this::fetchErrorMessage($code);	
+		}
+		
 		return self::encodeResponse(array('code' => $code, 'status' => 'success', 'message' => $message));
 	}
 	
-	public function fetchErrorMessage($code) {
-		
+	public function fetchErrorMessage($code) 
+	{
+		return constant(ErrorDefinition::$code);
 	}
 	
-	public function setResponseHeader($code) {
+	public function setResponseHeader($code) 
+	{
 		switch($code) {
 			default:
 				break;
 		}
 	}
 	
-	public static function encodeResponse(array $response) {
+	public static function encodeResponse(array $response) 
+	{
 		return json_encode($response);
 	}
+}
+
+class ErrorDefinition 
+{
+	const RESPONSE_SUCCESS = 'RESPONSE_SUCCESS';
+	
+	const PASSWORD_INVALID = 'The password provided is incorrect.';
+	const USER_UNVERIFIED = 'This user account has not yet been verified.';
+	
+	const STRIPE_INVALID_SUBSCRIPTION = 'No valid subscription found.';
 }
