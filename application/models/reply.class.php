125c939515862b5313b1381b5f47e6486e9c2a2e
Tom Humphris <tom@muska.co.uk>
2015-03-25 17:53:35 +0300

    Server side dates for new tickets and replies

--- application/models/reply.class.php
+++ application/models/reply.class.php
@@ -6,82 +6,84 @@ use Symfony\Component\HttpFoundation\Request;
 
 use Houston\Component\Helper;
 
-class ReplyModel 
+class ReplyModel
 {
 	protected $app;
 	public $reply;
-	
-	public function __construct(Application $app) 
+
+	public function __construct(Application $app)
 	{
 		$this->app = $app;
 	}
-	
-	public function loadReplyByID($replyID) 
+
+	public function loadReplyByID($replyID)
 	{
 		$connections = $this->app['mongo'];
 		$db = $connections['default'];
 		$db = $db->{$this->app['session']->get('database')};
-				
+
 		$this->reply = $db->replies->findOne(array('_id' => new \MongoID($id)));
-		
-		if(!empty($this->reply)) {			
+
+		if(!empty($this->reply)) {
 			return $this->reply;
 		} else {
 			throw new \Exception('Reply not found');
 		}
 	}
-	
-	public function getReplies($ticketID) 
+
+	public function getReplies($ticketID)
 	{
 		$connections = $this->app['mongo'];
 		$db = $connections['default'];
 		$db = $db->{$this->app['session']->get('database')};
-		
+
 		$ticketID = new \MongoID($ticketID);
-		
+
 		$collection = $db->replies;
 		$result = $collection->find(array('ticketID' => $ticketID));
-		
+
 		$docs = array();
 		foreach($result as $doc) {
 		    array_push($docs, $doc);
 		}
-		
+
 		return $docs;
 	}
-	
+
 	public function generateReply($ticketID, $message, $date = null, $email)
 	{
 		$this->reply = new \stdClass();
-		
+
 		$this->reply->ticketID = new \MongoID($ticketID);
 		$this->reply->message = $message;
 		$this->reply->date = (isset($date)) ? $date : Helper::convertTimestamp(date('Y-m-d H:i:s'));
-		
+
 		$userModel = new UserModel($this->app);
-		
+
 		try {
-			$userModel->loadUser($email);			
+			$userModel->loadUser($email);
 		} catch(\Exception $e) {
 			// User not found
 			throw new \Exception('Only registered users may generate a ticket reply');
 		}
-		
+
 		$this->reply->authorID = new \MongoID($userModel->user['_id']);
-		
+
 		return $this->reply;
 	}
-	
-	public function reply($reply) 
+
+	public function reply($reply)
 	{
 		$connections = $this->app['mongo'];
 		$db = $connections['default'];
 		$db = $db->{$this->app['session']->get('database')};
-		
+
 		$reply->ticketID = new \MongoID($reply->ticketID);
 		$reply->authorID = new \MongoID($reply->authorID);
-		
-		try {	
+
+		if(!isset($ticket->date)) $ticket->date = Helper::convertTimestamp(date('Y-m-d H:i:s'));
+
+		try {
 			$collection = $db->replies;
 			$collection->save($reply);
 			return $reply;
--- application/models/ticket.class.php
+++ application/models/ticket.class.php
@@ -3,95 +3,98 @@ namespace Houston\Model;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
+use Houston\Component\Helper;
 
-class TicketModel 
+class TicketModel
 {
 	protected $app;
 	public $ticket;
 
-	public function __construct(Application $app) 
-	{   
+	public function __construct(Application $app)
+	{
         $this->app = $app;
 	}
-	
-	public function loadTicketByID($ticketID) 
+
+	public function loadTicketByID($ticketID)
 	{
 		$connections = $this->app['mongo'];
 		$db = $connections['default'];
 		$db = $db->{$this->app['session']->get('database')};
-		
+
 		$ticketID = new \MongoID($ticketID);
-				
+
 		$this->ticket = $db->tickets->findOne(array('_id' => $ticketID));
-		
-		if(!empty($this->ticket)) {			
+
+		if(!empty($this->ticket)) {
 			return $this->ticket;
 		} else {
 			throw new \Exception('Ticket not found');
 		}
 	}
-	
-	public function getAll() 
+
+	public function getAll()
 	{
 		$connections = $this->app['mongo'];
 		$db = $connections['default'];
 		$db = $db->{$this->app['session']->get('database')};
-		
+
 		$tickets = $db->tickets;
 		$result = $tickets->find();
-		
+
 		$docs = array();
 		foreach($result as $doc) {
 		    array_push($docs, $doc);
 		}
-		
+
 		if(empty($docs)) {
 			return false;
 		} else {
-			return $docs;	
+			return $docs;
 		}
 	}
-	
-	public function generateTicket($subject, $message, $date = null, $email, $firstName = null, $lastName = null) 
+
+	public function generateTicket($subject, $message, $date = null, $email, $firstName = null, $lastName = null)
 	{
 		$this->ticket = new \stdClass();
-		
+
 		$this->ticket->subject = $subject;
 		$this->ticket->message = $message;
 		$this->ticket->date = (isset($date)) ? $date : Helper::convertTimestamp(date('Y-m-d H:i:s'));
 		$this->ticket->status = 'New';
-		
+
 		// Check to see if user account with email address already exists
 		$userModel = new UserModel($this->app);
-		
+
 		try {
-			$userModel->loadUser($email);			
+			$userModel->loadUser($email);
 		} catch(\Exception $e) {
 			// User not found so save new user
 			$user = new \stdClass();
 			$user->emailAddress = $email;
 			$user->firstName = $firstName;
 			$user->lastName = $lastName;
-			
+
 			$userModel->addUser($user);
-			
-			// Load the new user we created		
+
+			// Load the new user we created
 			$userModel->loadUser($email);
 		}
-		
+
 		$this->ticket->authorID = new \MongoID($userModel->user['_id']);
-		
+
 		return $this->ticket;
 	}
-		
-	public function add($ticket) 
+
+	public function add($ticket)
 	{
 		$connections = $this->app['mongo'];
 		$db = $connections['default'];
 		$db = $db->{$this->app['session']->get('database')};
-		
+
 		$ticket->authorID = new \MongoID($ticket->authorID);
-		
+
+		if(!isset($ticket->date)) $ticket->date = Helper::convertTimestamp(date('Y-m-d H:i:s'));
+
 		try {
 			$tickets = $db->tickets;
 			$tickets->save($ticket);
@@ -101,16 +104,16 @@ class TicketModel
 			return false;
 		}
 	}
-	
-	public function edit($ticket) 
+
+	public function edit($ticket)
 	{
 		$connections = $this->app['mongo'];
 		$db = $connections['default'];
 		$db = $db->{$this->app['session']->get('database')};
-		
-		try {	
+
+		try {
 			$id = new \MongoID($ticket->id);
-			
+
 			$tickets = $db->tickets;
 			$tickets->update(array('_id' => $id), $ticket);
 			return $ticket;
@@ -119,16 +122,16 @@ class TicketModel
 			return false;
 		}
 	}
-	
-	public function delete($ticketID) 
+
+	public function delete($ticketID)
 	{
 		$connections = $this->app['mongo'];
 		$db = $connections['default'];
 		$db = $db->{$this->app['session']->get('database')};
-		
+
 		$ticketID = new \MongoID($ticketID);
-		
-		try {	
+
+		try {
 			$collection = $db->tickets;
 			$collection->remove(array('_id' => $ticketID));
 			return true;
@@ -137,18 +140,18 @@ class TicketModel
 			return false;
 		}
 	}
-	
-	public function uploadAttachment($attachment) 
+
+	public function uploadAttachment($attachment)
 	{
 		$connections = $this->app['mongo'];
 		$db = $connections['default'];
 		$db = $db->{$this->app['session']->get('database')};
-		
+
 		// Remove MimeType from start of Base64 encoded binary string
 		$data = explode(',', $attachment->target);
 		$data = $data[1];
-		
-		try {	
+
+		try {
 			$gridfs = $db->getGridFS();
 			return $gridfs->storeBytes(base64_decode($data), array('contentType' => $attachment->type, 'fileName' => $attachment->name, 'lastModifiedDate' => $attachment->lastModifiedDate));
 		} catch(\MongoException $e) {
@@ -156,34 +159,34 @@ class TicketModel
 			return false;
 		}
 	}
-	
-	public function downloadAttachment($fileID) 
+
+	public function downloadAttachment($fileID)
 	{
 		$connections = $this->app['mongo'];
 		$db = $connections['default'];
 		$db = $db->{$this->app['session']->get('database')};
-			
-		$gridfs = $db->getGridFS();	
+
+		$gridfs = $db->getGridFS();
 		$file = $gridfs->findOne(array('_id' => new \MongoID($fileID)));
-		
+
 		if(!isset($file)) return false;
-		
+
 		$fileArr = array(
 			'data' => $file->getBytes(),
 			'fileName' => $file->file['fileName'],
 			'contentType' => $file->file['contentType'],
 			'lastModifiedDate' => $file->file['lastModifiedDate']
-		); 
-		
+		);
+
 		return $fileArr;
 	}
-	
+
 	public function deleteAttachment($fileID)
 	{
 		$connections = $this->app['mongo'];
 		$db = $connections['default'];
 		$db = $db->{$this->app['session']->get('database')};
-		
+
 		try {
 			$gridfs = $db->getGridFS();
 			return $gridfs->remove(array('_id' => new \MongoID($fileID)));
@@ -192,32 +195,32 @@ class TicketModel
 			return false;
 		}
 	}
-	
+
 	public function getFileMeta(Array $fileIDs)
 	{
 		$connections = $this->app['mongo'];
 		$db = $connections['default'];
 		$db = $db->{$this->app['session']->get('database')};
-		
+
 		$fileArr = array();
 		foreach($fileIDs as $fileID) {
 			$gridfs = $db->getGridFS();
-			$file = $gridfs->findOne(array('_id' => new \MongoID($fileID)));			
-			
+			$file = $gridfs->findOne(array('_id' => new \MongoID($fileID)));
+
 			$meta = array(
 				'_id' => $fileID,
 				'fileName' => $file->file['fileName'],
 				'contentType' => $file->file['contentType'],
 				'lastModifiedDate' => $file->file['lastModifiedDate']
 			);
-			
+
 			array_push($fileArr, $meta);
 		}
-		
+
 		if(empty($fileArr)) {
 			return false;
 		} else {
-			return $fileArr;	
+			return $fileArr;
 		}
 	}
 }
